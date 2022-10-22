caf40b4beb648b68ed01669f5b21c2a406039845
Neelesh2512 <neeleshsharma2512@gmail.com>
2022-10-23 01:15:16 +0530

    Feat: Added feature to add parties to list

---
+++ bill-printer/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AddPartyButton from './components/AddpartyButton';
+function App() {
+  return (
+    <div className="App">
+      <h1>BILL PRINTER</h1>
+      <AddPartyButton />
+    </div>
+  );
+}
+
+export default App;
